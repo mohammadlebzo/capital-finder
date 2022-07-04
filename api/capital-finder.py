321215803dc4a678cb256a132ba3a713d182ac7a
unknown <mohlebzo1@gmail.com>
2022-07-04 18:55:11 +0300

    testing

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -3,6 +3,12 @@ from urllib import parse
 import requests
 
 
+def q_handler(dic, key_word, url):
+    q_key = dic[key_word]
+    res = requests.get(url + q_key)
+    data = res.json()
+    return data
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
@@ -10,27 +16,28 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        url = 'https://restcountries.com/v3.1/name/'
         output = ''
 
-        # if 'country' in dic:
-        #     country = dic['country']
-        #     r = requests.get(url + country)
-        #     data = r.json()
-        #     for items in data:
-        #         output = f"The capital of {items['name']['common']} is {items['capital'][0]}."
-        #     message = output
+        if 'country' in dic:
+            # country = dic['country']
+            # url = 'https://restcountries.com/v3.1/name/'
+            # r = requests.get(url + country)
+            # data = r.json()
+            for items in q_handler(dic, "country", "https://restcountries.com/v3.1/name/"):
+                output = f"The capital of {items['name']['common']} is {items['capital'][0]}."
+            message = output
 
-        if 'capital' in dic:
-            capital = dic['capital']
-            r = requests.get(url + capital)
-            data = r.json()
-            for items in data:
+        elif 'capital' in dic:
+            # capital = dic['capital']
+            # url = 'https://restcountries.com/v2/capital/'
+            # r = requests.get(url + capital)
+            # data = r.json()
+            for items in q_handler(dic, "capital", "https://restcountries.com/v2/capital/"):
                 output = f"{items['capital'][0]} is the capital of {items['name']['common']}."
             message = output
 
-        # else:
-        #     message = "Please provide me with a word"
+        else:
+            message = "Please provide me with a word"
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
